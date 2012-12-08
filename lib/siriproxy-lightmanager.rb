d906d2c8c0ed2115298d2ccc1960bc94bfd9d022
Joep Verhaeg <info@joepverhaeg.nl>
2012-12-08 11:38:34 +0100

    Some code change and added randomized responses.

--- lib/siriproxy-lightmanager.rb
+++ lib/siriproxy-lightmanager.rb
@@ -2,9 +2,10 @@ require 'cora'
 require 'siri_objects'
 require 'pp'
 require 'socket'
+require 'time'
 
-# LightManager plugin v0.9 by Joep Verhaeg (info@joepverhaeg.nl)
-# Last update: Mar 5, 2012
+# LightManager plugin v1.0 by Joep Verhaeg (info@joepverhaeg.nl)
+# Last update: Dec 8, 2012
 #
 # Custom plugin for my home light scenes.
 #
@@ -13,51 +14,93 @@ require 'socket'
 class SiriProxy::Plugin::LightManager < SiriProxy::Plugin
   def initialize(config)
     # Appserver configuration (enable the appserver in Lightman Studio!)
+    # host = config["jb_host"]
+    # port = config["jb_port"]
+    
     @host = "192.168.1.1"
     @port = 6004
     
     # Receiver/scene id mapping
-    @receiver =  {'led' => '1','bookshelf' => '2','couch' => '3','kitchen' => '4','table' => '5','living' => '6'}
-    @scenes = {'TV' => '1','sleep' => '4','dinner' => '2','wake' => '3'}
+    @receiver =  {'led' => '1','bookshelf' => '2','couch' => '3','kitchen' => '4','table' => '5','living' => '6', 'radio' => '7', 'squeezebox' => '8', 'TV' => '10', 'door' => '11'}
+    @scenes = {'evening' => '1','wake' => '2','all' => '3','sleep' => '3','home' => '4','movie' => '5','leds' => '6'}
+    
+    @responses = [ "One moment.", "Your wish is my command.", "Just a second.", "OK.", "No problem.", "Hold on a second.", "Fine with me.", "Give me a second." ]
   end
  
   def kaku_device(device, command)
-    case command
-      when "off"
-        signal = "0"
-        say "I'll power off your " + device + " light!"
-      when "on"
-        signal = "255"
-        say "I'll power on your " + device + " light!"
-      else
-          signal = command
-          say "I'll dim your " + device + " light to " + command + "%"
-      end
-    socket = TCPSocket.open(@host,@port)
-    socket.puts("DEVICE~." + @receiver[device] + "~" + signal)
-    socket.close
-    request_completed
+    begin
+      case command
+        when "off"
+          signal = "0"
+          say "Powering off your " + device + " light.", spoken: @responses[rand(@responses.size)]
+        when "on"
+          signal = "255"
+          say "Powering on your " + device + " light.", spoken: @responses[rand(@responses.size)]
+        else
+            signal = command
+            say "Dimming your " + device + " light to " + command + "%", spoken: @responses[rand(@responses.size)]
+        end
+      socket = TCPSocket.open(@host,@port)
+      socket.puts("DEVICE~." + @receiver[device] + "~" + signal)
+      socket.close
+      request_completed
+    rescue Exception => e
+      say e.to_s, spoken: "Uh oh! Something bad happened..."
+      request_completed
+    end
+  end
+
+  def ir_device(device, command)
+    begin
+      case command
+        when "off"
+          signal = "0"
+          say "Turning off your " + device + ".", spoken: @responses[rand(@responses.size)]
+        when "on"
+          signal = "110"
+          say "Turning on your " + device + ".", spoken: @responses[rand(@responses.size)]
+        when "watch"
+          signal = "110"
+          say "Turning on your " + device + ".", spoken: @responses[rand(@responses.size)]
+        else
+          say "I do not understand you."
+        end
+      socket = TCPSocket.open(@host,@port)
+      socket.puts("DEVICE~." + @receiver[device] + "~" + signal)
+      socket.close
+      request_completed
+    rescue Exception => e
+      say e.to_s, spoken: "Uh oh! Something bad happened..."
+      request_completed
+    end
   end
 
   def kaku_scene(scene)
-    if scene == "sleep" then
-      say "See you later..."
-    else 
-    say "Starting your favorite " + scene + " light scene."
+    begin
+      if scene == "all" then
+        say "Powering off all your lights.", spoken: @responses[rand(@responses.size)]
+      elsif scene == "sleep" then
+        say "See you later..."
+      else 
+        say "Starting the " + scene + " scene.", spoken: @responses[rand(@responses.size)]
+      end
+      socket = TCPSocket.open(@host,@port)
+      socket.puts("SCENE~" + @scenes[scene])
+      socket.close
+      request_completed
+    rescue Exception => e
+      say e.to_s, spoken: "Uh oh! Something bad happened..."
+      request_completed
     end
-    socket = TCPSocket.open(@host,@port)
-    socket.puts("SCENE~" + @scenes[scene])
-    socket.close
-    request_completed
   end
 
   # Turn lights ON/OFF speaking scenario A
-  listen_for /(led|bookshelf|couch|kitchen|table|living).*(on|off)/i do |src, cmd|
+  listen_for /(led|bookshelf|couch|kitchen|table|living|door).*(on|off)/i do |src, cmd|
     kaku_device(src,cmd)
   end
   
   # Turn lights ON/OFF speaking scenario B
-  listen_for /(on|off).*(led|bookshelf|couch|kitchen|table|living)/i do |cmd, src|
+  listen_for /(on|off).*(led|bookshelf|couch|kitchen|table|living|door)/i do |cmd, src|
     kaku_device(src,cmd)
   end
 
@@ -73,12 +116,12 @@ class SiriProxy::Plugin::LightManager < SiriProxy::Plugin
   end
 
   # Launch a light scene speaking scenario A
-  listen_for /(tv|dinner|wake).*scene/i do |src|
+  listen_for /(evening|all|dinner|wake|home|movie|leds).*light/i do |src|
     kaku_scene(src)
   end
 
   # Launch a light scene speaking scenario B
-  listen_for /scene.*(tv|dinner|wake)/i do |src|
+  listen_for /light.*(evening|all|dinner|wake|home|movie|leds)/i do |src|
     kaku_scene(src)
   end
 
@@ -91,5 +134,15 @@ class SiriProxy::Plugin::LightManager < SiriProxy::Plugin
       say "See you later..."
       request_completed
     end
+  end
+
+  # Turn IR device ON/OFF speaking scenario A
+  listen_for /(TV|radio|squeezebox).*(on|off|watch)/i do |src, cmd|
+    ir_device(src,cmd)
+  end
+  
+  # Turn IR device ON/OFF speaking scenario B
+  listen_for /(on|off|watch).*(TV|radio|squeezebox)/i do |cmd, src|
+    ir_device(src,cmd)
   end
 end
